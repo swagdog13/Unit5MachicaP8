17dfc5e740d4636ff37071c1932aeb6856a46c3b
swagdog13 <727159@student.dusd.net>
2025-05-20 15:26:27 -0700

    rESTARY
    I added a restart button

--- MachicaP8Unit5/Assets/Scripts/GameManager.cs
+++ MachicaP8Unit5/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,11 +11,14 @@ public class GameManager : MonoBehaviour
     private float spawnRate = 1.0f;
     private int score;
     public TextMeshProUGUI scoreText;
-
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
 
     // Start is called before the first frame update
     void Start() 
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
@@ -27,7 +32,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -39,5 +44,17 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
--- MachicaP8Unit5/Assets/Scripts/Target.cs
+++ MachicaP8Unit5/Assets/Scripts/Target.cs
@@ -33,15 +33,24 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
+        if(gameManager.isGameActive)
+        
         Destroy(gameObject);
         Instantiate(explosionParticle, transform.position, 
         explosionParticle.transform.rotation);
         gameManager.UpdateScore(pointValue);
     }
 
+
+
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
 
     Vector3 RandomForce()
